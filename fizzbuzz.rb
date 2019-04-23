134c4c5c281cf6ca36caaad9c62aca9b4c450c15
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-04-23 19:33:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  num3 = num % 3
-  num5 = num % 5
-  if num3 + num5 == 0
-    puts "FizzBuzz"
-  elsif num3 == 0
-  puts "Fizz"
-  elsif num5 == 0
-  puts "Buzz"
-else return nil
-end  
+  if num % 3 == 0 
+    puts "Fizz"
+    elsif num % 5 == 0
+    puts "Buzz"
+  end
